539863dd6b9dbcdec7da06081118a22d654416d0
faizaladin <faizaladin365@gmail.com>
2025-07-21 18:27:37 -0700

    test

--- test.py
+++ test.py
@@ -1,21 +1,16 @@
 import torch
 from diffusers import AutoPipelineForImage2Image
-from diffusers.utils import make_image_grid, load_image
+from diffusers.utils import load_image, make_image_grid
 
 pipeline = AutoPipelineForImage2Image.from_pretrained(
-    "stable-diffusion-v1-5/stable-diffusion-v1-5", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+    "kandinsky-community/kandinsky-2-2-decoder", torch_dtype=torch.float16, use_safetensors=True
 )
 pipeline.enable_model_cpu_offload()
 # remove following line if xFormers is not installed or you have PyTorch 2.0 or higher installed
 pipeline.enable_xformers_memory_efficient_attention()
 
-# prepare image
 init_image = load_image("frame_0000.png")
 
-prompt = "Make this image more photorealistic"
-
-# pass prompt and image to pipeline
-image = pipeline(prompt, image=init_image, strength=0.3).images[0]
-
-# save the generated image
+prompt = "Make this photo more realistic, 4k"
+image = pipeline(prompt, image=init_image).images[0]
 image.save("output.png")

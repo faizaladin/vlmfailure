c5809d69298b94f411894a65912311a4dae20b3f
faizaladin <faizaladin365@gmail.com>
2025-07-27 18:27:18 -0400

    test success

--- inference.py
+++ inference.py
@@ -5,17 +5,15 @@ from PIL import Image
 # Load the model in half-precision
 model = LlavaForConditionalGeneration.from_pretrained("llava-hf/llava-1.5-7b-hf", torch_dtype=torch.float16, device_map="auto")
 processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
-image1 = Image.open("paired_fail_4k.png")
-image2 = Image.open("paired_success_4k.png")
+image = Image.open("paired_success_4k.png")
 
 
 conversation = [
     {
         "role": "user",
         "content": [
-            {"type": "image", "image": image1},
-            {"type": "image", "image": image2},
-            {"type": "text", "text": "These are two paired images of cars using a vision based algorithm to steer under two different weather conditions. The weather conditions on both left images are exactly the same and the weather conditions on the right images are exactly the same. The first pair ends in failure and the second pair succeeds. Please explain why the car crashes in the first pair of images but not in the second pair of images, despite being under the same weather conditions. Create a list of these failures and provide bullet points of reasoning for each one. Lastly, see if there are similar failures that could arise in different weather conditions."},
+            {"type": "image", "image": image},
+            {"type": "text", "text": "This is a paired image of two cars using a vision based algorithm to steer under different weather conditions. Is there a cause of failure in this image, and if so what is the cause of failure? Please give a yes or no answer followed by reasoning specific to the image and pertains to the weather condition. Create a list of these failures and provide bullet points of reasoning for each one. Lastly, see if there are similar failures that could arise in different weather conditions."},
         ],
     },
 ]

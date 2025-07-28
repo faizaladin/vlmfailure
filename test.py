4978939f8536d48692c1a0b8a3b9258441deaef5
faizaladin <faizaladin365@gmail.com>
2025-07-28 13:04:55 -0400

    test

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-from PIL import Image
-img = Image.open("success_rain.png")
-print(img.size)  # (width, height)
+from PIL import Image, UnidentifiedImageError
+
+try:
+    img = Image.open('paired_frames/pos_0.9999999999999998_head_42/frame_00002.png')
+    print("Image is valid.")
+    print(img.size)  # (width, height)
+except UnidentifiedImageError:
+    print("Image is not valid or cannot be opened.")
